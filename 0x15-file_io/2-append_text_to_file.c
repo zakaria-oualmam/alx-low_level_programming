86984950dd22fd3ef5acfee74f7926fb35e861c7
zakaria-oualmam <zoualmam@gmail.com>
2023-08-07 07:05:56 -0700

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,37 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
 
 /**
- * append_text_to_file - that appends text at the end of a file
- * @filename: variable pointer
- * @text_content: content file
- * Description: function that appends text at the end of a file
- * Return: 1 on success, -1 on failure
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
+ *
+ * Return: the length of str, or -1 if str is NULL
  */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
 
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * append_text_to_file - append text to the end of a file
+ * @filename: the name of the file to append to
+ * @text_content: the data to append to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	ssize_t b_written = 0;
+	int fd;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename, O_WRONLY | O_APPEND);
-
-	if (file == -1)
+	if (fd < 0)
 		return (-1);
 
-	write(file, text_content, i);
+	if (text_content)
+		b_written = write(fd, text_content, _strlen(text_content));
 
+	close(fd);
+
+	if (b_written < 0)
+		return (-1);
 	return (1);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,9 +1,9 @@
-#ifndef FILE_IO
-#define FILE_IO
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int _putchar(char c);
@@ -11,4 +11,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /* HOLBERTON_H */
